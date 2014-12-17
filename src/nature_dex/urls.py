442c235ece691c511727c30fda0db8fa01fac5c8
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-12-17 14:54:27 +0100

    Update settings for mediafiles

--- src/nature_dex/urls.py
+++ src/nature_dex/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.gis import admin as gisadmin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework.routers import DefaultRouter
 from nature_dex.views import SpecimenViewSet, GroupViewSet
@@ -17,3 +19,23 @@ urlpatterns = patterns('',
     url(r'^', include(router.urls)),
     url(r'^admin/', include(gisadmin.site.urls)),
 )
+
+def mediafiles_urlpatterns():
+    """
+    Method for serve media files with runserver.
+    """
+
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith("/"):
+        _media_url = _media_url[1:]
+
+    from django.views.static import serve
+    return [
+        url(r"^%s(?P<path>.*)$" % _media_url, serve,
+            {"document_root": settings.MEDIA_ROOT})
+    ]
+
+
+# Static & Media
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += mediafiles_urlpatterns()
