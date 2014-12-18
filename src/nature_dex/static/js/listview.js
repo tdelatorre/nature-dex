8a4aee71a89f05b51b49e318ccf052c4f2872493
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-12-18 17:20:39 +0100

    Add html in no result

--- src/nature_dex/static/js/listview.js
+++ src/nature_dex/static/js/listview.js
@@ -15,7 +15,7 @@
         if ( data.specimenes.previous === null) {
             _page = 1;
         }
-        else if ( data.specimenes.next === null ) { 
+        else if ( data.specimenes.next === null ) {
             var preg = data.specimenes.previous.split('=');
             _page = Number(preg[preg.length - 1]) + 1;
         }
@@ -25,31 +25,38 @@
         }
 
         console.log( 'ListView::Services::', data );
+        console.log(data.specimenes.results);
 
-        for ( var specimen in data.specimenes.results) {
-            var obj = data.specimenes.results[specimen];
+        if (!data.specimenes.results.length) {
+            var html = '<p>No hemos encontrado nada</p>';
+            $( '#listview' ).append( html );
 
-            // Datos para renderizarcontent-list
-            var image = (obj.specimen_image !== null) ? obj.specimen_image : Services.getLocation() + 'static/images/animal_fallback.jpg';
-            var specimenId = obj.id;
-            var commonName = obj.common_name;
-            var scientificName = obj.scientific_name;
-            var html = '';
-            html += '<li data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c">';
-            html += '    <div class="ui-btn-inner ui-li" aria-hidden="true">';
-            html += '        <div class="ui-btn-text">';
-            html += '            <a href="#" class="ui-link-inherit" data-specimen-id="' + specimenId + '">';
-            html += '                <img src="' + image + '" class="ui-li-thumb">';
-            html += '                <h3 class="ui-li-heading">' + scientificName + '</h3>';
-            html += '                <p class="ui-li-desc">' + commonName + '</p>';
-            html += '            </a>';
-            html += '        </div>';
-            html += '        <span class="ui-icon ui-icon-arrow-r ui-icon-shadow"></span>';
-            html += '    </div>';
-            html += '</li>';
+        } else {
+            for ( var specimen in data.specimenes.results) {
+                var obj = data.specimenes.results[specimen];
 
-            console.log( 'specimen', obj.scientific_name );
-            _$view.find( '#content-list' ).append( html );
+                // Datos para renderizarcontent-list
+                var image = (obj.specimen_image !== null) ? obj.specimen_image : Services.getLocation() + 'static/images/animal_fallback.jpg';
+                var specimenId = obj.id;
+                var commonName = obj.common_name;
+                var scientificName = obj.scientific_name;
+                var html = '';
+                html += '<li data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c">';
+                html += '    <div class="ui-btn-inner ui-li" aria-hidden="true">';
+                html += '        <div class="ui-btn-text">';
+                html += '            <a href="#" class="ui-link-inherit" data-specimen-id="' + specimenId + '">';
+                html += '                <img src="' + image + '" class="ui-li-thumb">';
+                html += '                <h3 class="ui-li-heading">' + scientificName + '</h3>';
+                html += '                <p class="ui-li-desc">' + commonName + '</p>';
+                html += '            </a>';
+                html += '        </div>';
+                html += '        <span class="ui-icon ui-icon-arrow-r ui-icon-shadow"></span>';
+                html += '    </div>';
+                html += '</li>';
+
+                console.log( 'specimen', obj.scientific_name );
+                _$view.find( '#content-list' ).append( html );
+            }
         }
 
         // Events
@@ -60,11 +67,11 @@
     }
 
     function onSpecimenClick ( event ) {
-        
+
         var specimenId = $(this).attr( 'data-specimen-id' );
 
         SpecimenById.getSpecimenById(specimenId);
-        
+
         event.preventDefault();
     }
 
