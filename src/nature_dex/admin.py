66624f71363d52f9da2df8f2d98bb6c023d1cbcd
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-12-18 15:39:13 +0100

    Update admin

--- src/nature_dex/admin.py
+++ src/nature_dex/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.contrib.gis import admin as gisadmin
 from .models import *
 
+class SpecimenAdmin(admin.ModelAdmin):
+    search_fields = ('scientific_name', 'common_name')
+    list_filter = ('kingdom', 'group')
 
 admin.site.register(Zone, gisadmin.GeoModelAdmin)
-admin.site.register(Specimen)
+admin.site.register(Specimen, SpecimenAdmin)
 admin.site.register(SpecimenByZone)
