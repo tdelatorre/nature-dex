f53e99e5dd324f7d835d72f913aca85ffad436fa
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2015-07-17 10:28:13 +0200

    Add comments in code

--- insert_zones.sql
+++ insert_zones.sql
@@ -1,2 +1,4 @@
+-- ~ Insert fields in nature_dex_zone from net data base.
+
 INSERT INTO nature_dex_zone (ext_ref, marine, mpoly) SELECT cuadricula, bool(marino), geom FROM malla10x10_p;
 INSERT INTO nature_dex_zone (ext_ref, marine, mpoly) SELECT cuadricula, bool(marino), geom FROM malla10x10_c;
